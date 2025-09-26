bd968cd9afb251587b77bccfa562f7708ee17972
Luke22Skull <lukashi2209@gmail.com>
2025-09-26 09:02:27 +0200

    Initial commit

---
+++ src/parallax.js
@@ -0,0 +1,22 @@
+let ticking = false;
+
+window.addEventListener('scroll', function() {
+    if (!ticking) {
+        window.requestAnimationFrame(function() {
+            const bodySection = document.querySelector('body');
+            const scrollPosition = window.scrollY;
+            const parallaxSpeed = 0.85; // Velocità dell'effetto parallasse
+
+            if (window.innerWidth > 768) {
+                let parallaxOffset = scrollPosition * parallaxSpeed;
+                bodySection.style.backgroundPositionY = `${parallaxOffset}px`;
+            } else {
+                bodySection.style.backgroundPositionY = 'center';
+            }
+
+            ticking = false;
+        });
+
+        ticking = true;
+    }
+});
